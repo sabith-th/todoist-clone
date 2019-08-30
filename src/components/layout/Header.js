7b9a8df13d691557aa80936501702c33fcbe12eb
SabithHaneef <sabith.th.07@gmail.com>
2019-08-30 19:12:08 +0530

    initial project setup, cleanup boilerplate

---
+++ src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header" data-testid="header">
+    <nav>
+      <div className="logo">
+        <img src="" alt="Todoist" />
+      </div>
+    </nav>
+  </header>
+);
+
+export default Header;
