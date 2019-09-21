8f2d9f745b02c15f27a5387a9677ca778fe81f1e
SabithHaneef <sabith.th.07@gmail.com>
2019-09-21 15:38:58 +0530

    added ProjectOverlay and TaskDate components

---
+++ src/components/ProjectOverlay.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React from 'react';
+import { useProjectsValue } from '../context';
+
+const ProjectOverlay = ({
+  setProject,
+  showProjectOverlay,
+  setShowProjectOverlay,
+}) => {
+  const { projects } = useProjectsValue();
+
+  return (
+    projects
+    && showProjectOverlay && (
+      <div className="project-overlay" data-testid="project-overlay">
+        <ul className="project-overlay__list">
+          {projects.map((project) => (
+            <li
+              key={project.projectId}
+              data-testid="project-overlay-action"
+              onClick={() => {
+                setProject(project.projectId);
+                setShowProjectOverlay(false);
+              }}
+            >
+              {project.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  );
+};
+
+export default ProjectOverlay;
---
+++ src/components/TaskDate.js
@@ -0,0 +1,58 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import moment from 'moment';
+import React from 'react';
+import { FaRegPaperPlane, FaSpaceShuttle, FaSun } from 'react-icons/fa';
+
+const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) => showTaskDate && (
+    <div className="task-date" data-testid="task-date-overlay">
+      <ul className="task-date__list">
+        <li
+          onClick={() => {
+            setShowTaskDate(false);
+            setTaskDate(moment().format('DD-MM-YYYY'));
+          }}
+          data-testid="task-date-today"
+        >
+          <span>
+            <FaSpaceShuttle />
+          </span>
+          <span>Today</span>
+        </li>
+        <li
+          onClick={() => {
+            setShowTaskDate(false);
+            setTaskDate(
+              moment()
+                .add(1, 'day')
+                .format('DD-MM-YYYY'),
+            );
+          }}
+          data-testid="task-date-tomorrow"
+        >
+          <span>
+            <FaSun />
+          </span>
+          <span>Tomorrow</span>
+        </li>
+        <li
+          onClick={() => {
+            setShowTaskDate(false);
+            setTaskDate(
+              moment()
+                .add(7, 'days')
+                .format('DD-MM-YYYY'),
+            );
+          }}
+          data-testid="task-date-next-week"
+        >
+          <span>
+            <FaRegPaperPlane />
+          </span>
+          <span>Next Week</span>
+        </li>
+      </ul>
+    </div>
+  );
+
+export default TaskDate;
